44fe32be536b63dac87a3652568e6a14c751b43f
Dinushika Sandamali <66713238+Dinushika916@users.noreply.github.com>
2020-07-09 13:05:43 +0530

    about Added

---
+++ about.php
@@ -0,0 +1,98 @@
+<?php
+include_once "headerHTML.php";
+include_once "navigation.php";
+?>
+
+<!-- HTML code goes here -->
+<body>
+<div class="container bg-white">
+    <section class="contact-details section-gap">
+        <div class="row justify-content-center">
+            <span class="justify-content-between text-secondary mr-3 d-none d-md-block">__________________</span>
+            <h2 class="heading">About Us</h2>
+            <span class="justify-content-between text-secondary ml-3 d-none d-md-block">__________________</span>
+        </div>
+    </section>
+
+  
+    <div>
+    <div class="about-image"></div>
+    </div>
+
+    <br>
+    <br>
+    
+    <div>
+      <section class="index">
+        <div>
+            <p>
+    		Avenra Hotels is a public quoted company which entered the leisure industry with a commitment to build and manage a chain of distinctive hotels and resorts that are benchmarked against the most coveted in the world. Avenra intends to provide the most exceptional and memorable hospitality in Sri Lanka by exceeding guest expectations and by creating a travel experience like no other.
+            </p>
+
+            <p>
+            Avenra currently owns three properties in Sri Lanka.Avenra Colombo is a 90 room hotel situated in the heart of Colombo. Ceremoniously opened in 2014, Avenra Negombo is a 5 star resort in the West Coast of Sri Lanka comprising of 150 rooms with luxury amenities. The Steuart by Avenra is the latest luxury Boutique Business Hotel comprising of 50 rooms, situated on eight floors capturing the mystery of Galle, embedded by history dating back to 1835.
+            </p>
+        </div>
+
+        <div>
+        	<h5><b>VISION</b></h5>
+        	<p>Our vision is to be the ultimate hospitality brand in Sri Lanka.</p>
+
+        </div>
+
+        <div>    
+            <h5><b>MISSION</b></h5>
+
+            <p>Our mission is to own and manage hotels and resorts in Sri Lanka where we can deliver exceptional hospitality by going beyond the call of duty and extending a personalized, friendly and professional service at every given opportunity.</p>
+        </div>
+
+
+    </section>
+
+    <br>
+    <br>
+
+    <section class="label">
+	    <div class="row">
+            <div class="column" >
+            	 <span class="fa-stack fa-4x">
+                    <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                    <i class="fa fa-bed fa-stack-1x"></i>
+                 </span>
+                 <h1>100</h1>
+                 <h3>Rooms</h3>
+                
+            </div>
+            <div class="column">
+            	    <span class="fa-stack fa-4x">
+                    <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                    <i class="fa fa-bath fa-stack-1x"></i>
+                    </span>
+                    <h2>80</h2>
+                    <h3> Bathrooms </h3>
+            </div>
+            <div class="column">
+            	<span class="fa-stack fa-4x">
+                    <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                    <i class="fa fa-car fa-stack-1x"></i>
+                 </span>
+                 <h2>2+</h2>
+                 <h3> Parks </h3>
+            </div>
+            <div class="column">
+            	<span class="fa-stack fa-4x">
+                    <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                    <i class="fa fa-map-marker fa-stack-1x"></i>
+                 </span>
+                 <h2>2000+</h2>
+                 <h3> Size in SQFT </h3>
+            </div>
+        </div>
+        </section>
+    
+</div>
+</body>
+
+<?php
+include_once "footerHTML.php";
+?>
