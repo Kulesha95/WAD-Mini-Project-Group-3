975aad75576829e838f4e9a4906c3a0d6b47543b
Madushi <madushi97pabasara@gmail.com>
2020-07-09 17:50:44 +0530

    accomodation page created

--- accomodation.php
+++ accomodation.php
@@ -3,9 +3,9 @@ include_once "headerHTML.php";
 include_once "navigation.php";
 
 ?>
-
-<section id="roomDesc">
-		<div><img src="img/main-room.jpg" style="object-fit: none;width: 1348px"></div>
+<div class="container">
+	<section id="roomDesc">
+		<div><img src="img/main-room.jpg" style="width:100%;height:300px;object-fit:cover"></div>
 		<article class="roomArticle">
         	<div class="col-12 col-md-6 mb-3">
                 <header class="head">
@@ -19,182 +19,179 @@ include_once "navigation.php";
             </div>
         </article>
 	</section>
+
 	<section id="rooms">
-	<div class="container" id="container">
-        <div class="row rowRoom">
-            <div class="col">
-            	<a href="deluxe-ocean-view.html" title="Deluxe Ocean View"><img src="img/thumb-deluxe-ocean-view.jpg" class="img"></a>
-            </div>
-            <div class="col">
-            	<div class="content">
-            		<div class="title">Rooms</div>
-                	<div class="name">
-                		<h3><a href="deluxe-ocean-view.html" title="Deluxe Ocean View" class="stayType">Deluxe Ocean View</a></h3></div><hr>
-                	<div class="description">
-                    	<ul class="descriptUL">
-                        	<li><b>Room Size:</b>  39 sqm.</li>
-                        	<li><b>View:</b>  Ocean view</li>
-                        	<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
-                        	<li><b>Bed Type(s):</b>  King or twin beds</li>
-                        	<li><b>Inter-connecting Rooms:</b>  Available</li>
-                    	</ul>
+		<div class="container" id="container">
+<!--room1-->
+        	<div class="row rowRoom">
+            	<div class="col-12 col-lg-6 order-1 order-lg-1">
+            		<a href="#" title="Deluxe Ocean View"><img src="img/thumb-deluxe-ocean-view.jpg" class="img"></a>
+            	</div>
+            	<div class="col-12 col-lg-6 order-2 order-lg-2">
+            		<div class="content">
+            			<div class="title">Rooms</div>
+                		<div class="name">
+                			<h3><a href="#" title="Deluxe Ocean View" class="stayType">Deluxe Ocean View</a></h3></div><hr>
+                		<div class="description">
+                    		<ul class="descriptUL">
+                        		<li><b>Room Size:</b>  39 sqm.</li>
+                        		<li><b>View:</b>  Ocean view</li>
+                        		<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
+                        		<li><b>Bed Type(s):</b>  King or twin beds</li>
+                        		<li><b>Inter-connecting Rooms:</b>  Available</li>
+                    		</ul>
+               			</div>
+               			<div class="button">
+                        	<a class="btn" href="#" title="View Deluxe Ocean View" style="background-color: #C81168">View</a>
+                        	<a class="btn" href="#" title="Book Deluxe Ocean View">Book Now</a>
+                    	</div>
                		</div>
-               		<div class="button">
-                        <a class="btn" href="deluxe-ocean-view.html" title="View Deluxe Ocean View" style="background-color: #C81168">View</a>
-                        <a class="btn" href="#" title="Book Deluxe Ocean View">Book Now</a>
-                    </div>
-               	</div>
-           	</div>
-       	</div>
-       	<br><br>
+           		</div>
+       		</div>
+       		<br><br>
 
-        <div class="row rowRoom">
-            <div class="col">
-            	<div class="content">
-            		<div class="title">Rooms</div>
-                	<div class="name">
-                		<h3><a href="deluxe-ocean-horizon.html" title="Deluxe Ocean View Horizon" class="stayType">Deluxe Ocean View Horizon</a>
-                		</h3>
-                	</div><hr>
-                	<div class="description">
-                    	<ul class="descriptUL">
-                        	<li><b>Room Size:</b>  39 sqm.</li>
-                        	<li><b>View:</b>  Ocean view</li>
-                        	<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
-                        	<li><b>Bed Type(s):</b>  King or twin beds</li>
-                        	<li><b>Inter-connecting Rooms:</b>  Available</li>
-                    	</ul>
+<!--room2-->
+        	<div class="row rowRoom">
+				<div class="col-12 col-lg-6 order-1 order-lg-2">
+            		<a href="#" title="Deluxe Ocean View Horizon"><img src="img/thumb-deluxe-ocean-horizon.jpg" class="img"></a>
+            	</div>
+            	<div class="col-12 col-lg-6 order-2 order-lg-1" >
+            		<div class="content">
+            			<div class="title">Rooms</div>
+                		<div class="name">
+                			<h3><a href="#" title="Deluxe Ocean View Horizon" class="stayType">Deluxe Ocean View Horizon</a></h3>
+                		</div><hr>
+                		<div class="description">
+                    		<ul class="descriptUL">
+                        		<li><b>Room Size:</b>  39 sqm.</li>
+                        	 	<li><b>View:</b>  Ocean view</li>
+                        		<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
+                        		<li><b>Bed Type(s):</b>  King or twin beds</li>
+                        		<li><b>Inter-connecting Rooms:</b>  Available</li>
+                    		</ul>
+               			</div>
+               			<div class="button">
+                        	<a class="btn" href="#" title="View Deluxe Ocean View Horizon" style="background-color: #C81168">View</a>
+                        	<a class="btn" href="#" title="Book Deluxe Ocean View Horizon">Book Now</a>
+                    	</div>
                		</div>
-               		<div class="button">
-                        <a class="btn" href="deluxe-ocean-horizon.html" title="View Deluxe Ocean View Horizon" style="background-color: #C81168">View</a>
-                        <a class="btn" href="#" title="Book Deluxe Ocean View Horizon">Book Now</a>
-                    </div>
-               	</div>
-            </div>
-            <div class="col">
-            	<a href="deluxe-ocean-horizon.html" title="Deluxe Ocean View Horizon"><img src="img/thumb-deluxe-ocean-horizon.jpg" class="img"></a>
-            </div>
-        </div>
-        <br><br>
-
-        <div class="row rowRoom">
-            <div class="col">
-            	<a href="grand-deluxe-ocean-view.html" title="Grand Deluxe Ocean View">
-            		<img src="img/thumb-grand-deluxe-ocean-view.jpg" class="img"></a>
-            </div>
-            <div class="col">
-            	<div class="content">
-            		<div class="title">Rooms</div>
-                	<div class="name">
-                		<h3><a href="grand-deluxe-ocean-view.html" title="Grand Deluxe Ocean View" class="stayType">Grand Deluxe Ocean View</a></h3>
-                	</div><hr>
-                	<div class="description">
-                    	<ul class="descriptUL">
-                        	<li><b>Room Size:</b>  57 sqm.</li>
-                        	<li><b>View:</b>  Ocean view</li>
-                        	<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
-                        	<li><b>Bed Type(s):</b>  King bed</li>
-                        	<li><b>Inter-connecting Rooms:</b>  Available</li>
-                    	</ul>
+            	</div>
+        	</div>
+        	<br><br>
+<!--room3-->
+        	<div class="row rowRoom">
+            	<div class="col-12 col-lg-6 order-1 order-lg-1">
+            		<a href="#" title="Grand Deluxe Ocean View"><img src="img/thumb-grand-deluxe-ocean-view.jpg" class="img"></a>
+            	</div>
+            	<div class="col-12 col-lg-6 order-2 order-lg-2">
+            		<div class="content">
+            			<div class="title">Rooms</div>
+                		<div class="name">
+                			<h3><a href="#" title="Grand Deluxe Ocean View" class="stayType">Grand Deluxe Ocean View</a></h3>
+                		</div><hr>
+                		<div class="description">
+                    		<ul class="descriptUL">
+                        		<li><b>Room Size:</b>  57 sqm.</li>
+                        		<li><b>View:</b>  Ocean view</li>
+                        		<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
+                        		<li><b>Bed Type(s):</b>  King bed</li>
+                        		<li><b>Inter-connecting Rooms:</b>  Available</li>
+                    		</ul>
+               			</div>
+               			<div class="button">
+                        	<a class="btn" href="#" title="View Grand Deluxe Ocean View" style="background-color: #C81168">View</a>
+                        	<a class="btn" href="#" title="Book Grand Deluxe Ocean View">Book Now</a>
+                    	</div>
                		</div>
-               		<div class="button">
-                        <a class="btn" href="grand-deluxe-ocean-view.html" title="View Grand Deluxe Ocean View" style="background-color: #C81168">View</a>
-                        <a class="btn" href="#" title="Book Grand Deluxe Ocean View">Book Now</a>
-                    </div>
-               	</div>
-            </div>
-        </div>
-        <br><br>
-
-        <div class="row rowRoom">
-            <div class="col">
-            	<div class="content">
-            		<div class="title">Suites</div>
-                	<div class="name">
-                		<h3><a href="junior-suite-ocean-view.html" title="Junior Suite Ocean View" class="stayType">Junior Suite Ocean View</a></h3>
-                	</div><hr>
-                	<div class="description">
-                    	<ul class="descriptUL">
-                        	<li><b>Room Size:</b>  57 sqm.</li>
-                        	<li><b>View:</b>  Ocean view</li>
-                        	<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
-                        	<li><b>Bed Type(s):</b>  King bed</li>
-                        	<li><b>Inter-connecting Rooms:</b>  Not Available</li>
-                    	</ul>
+            	</div>
+        	</div>
+        	<br><br>
+<!--suite1-->
+        	<div class="row rowRoom">
+				<div class="col-12 col-lg-6 order-1 order-lg-2">
+            		<a href="#" title="Junior Suite Ocean View"><img src="img/thumb-junior-suite-ocean-view.jpg" class="img"></a>
+            	</div>
+            	<div class="col-12 col-lg-6 order-2 order-lg-1">
+            		<div class="content">
+            			<div class="title">Suites</div>
+                		<div class="name">
+                			<h3><a href="#" title="Junior Suite Ocean View" class="stayType">Junior Suite Ocean View</a></h3>
+                		</div><hr>
+                		<div class="description">
+                    		<ul class="descriptUL">
+                        		<li><b>Room Size:</b>  57 sqm.</li>
+                        		<li><b>View:</b>  Ocean view</li>
+                        		<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
+                        		<li><b>Bed Type(s):</b>  King bed</li>
+                        		<li><b>Inter-connecting Rooms:</b>  Not Available</li>
+                    		</ul>
+               			</div>
+               			<div class="button">
+                        	<a class="btn" href="#" title="View Junior Suite Ocean View" style="background-color: #C81168">View</a>
+                        	<a class="btn" href="#" title="Book Junior Suite Ocean View">Book Now</a>
+                    	</div>
                		</div>
-               		<div class="button">
-                        <a class="btn" href="junior-suite-ocean-view.html" title="View Junior Suite Ocean View" style="background-color: #C81168">View</a>
-                        <a class="btn" href="#" title="Book Junior Suite Ocean View">Book Now</a>
-                    </div>
-               	</div>
+            	</div>
             </div>
-            <div class="col">
-            	<a href="junior-suite-ocean-view.html" title="Junior Suite Ocean View">
-            		<img src="img/thumb-junior-suite-ocean-view.jpg" class="img"></a>
-            </div>
-        </div>
-        <br><br>
-
-        <div class="row rowRoom">
-            <div class="col">
-            	<a href="one-bedroom-suite.html" title="One Bedroom Suite">
-            		<img src="img/thumb-one-bedroom-suite.jpg" class="img"></a>
-            </div>
-            <div class="col">
-            	<div class="content">
-            		<div class="title">Suites</div>
-                	<div class="name">
-                		<h3><a href="one-bedroom-suite.html" title="One Bedroom Suite" class="stayType">One Bedroom Suite
-                		</a></h3>
-                	</div><hr>
-                	<div class="description">
-                    	<ul class="descriptUL">
-                        	<li><b>Room Size:</b>  95 sqm.</li>
-                        	<li><b>View:</b>  Ocean view</li>
-                        	<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
-                        	<li><b>Bed Type(s):</b>  King bed</li>
-                        	<li><b>Inter-connecting Rooms:</b>  Not Available</li>
-                    	</ul>
+        	<br><br>
+<!--suite2-->
+        	<div class="row rowRoom">
+            	<div class="col-12 col-lg-6 order-1 order-lg-1">
+            		<a href="#" title="One Bedroom Suite"><img src="img/thumb-one-bedroom-suite.jpg" class="img"></a>
+            	</div>
+            	<div class="col-12 col-lg-6 order-2 order-lg-2">
+            		<div class="content">
+            			<div class="title">Suites</div>
+                		<div class="name">
+                			<h3><a href="#" title="One Bedroom Suite" class="stayType">One Bedroom Suite</a></h3>
+                		</div><hr>
+                		<div class="description">
+                    		<ul class="descriptUL">
+                        		<li><b>Room Size:</b>  95 sqm.</li>
+                        		<li><b>View:</b>  Ocean view</li>
+                        		<li><b>Ideal For:</b>  2 adults and 1 child or 3 adults</li>
+                        		<li><b>Bed Type(s):</b>  King bed</li>
+                        		<li><b>Inter-connecting Rooms:</b>  Not Available</li>
+                    		</ul>
+               			</div>
+               			<div class="button">
+                        	<a class="btn" href="#" title="View One Bedroom Suite" style="background-color: #C81168">View</a>
+                        	<a class="btn" href="#" title="Book One Bedroom Suite">Book Now</a>
+                    	</div>
                		</div>
-               		<div class="button">
-                        <a class="btn" href="one-bedroom-suite.html" title="View One Bedroom Suite" style="background-color: #C81168">View</a>
-                        <a class="btn" href="#" title="Book One Bedroom Suite">Book Now</a>
-                    </div>
-               	</div>
-            </div>
-        </div>
-        <br><br>
-
-        <div class="row rowRoom">
-            <div class="col">
-            	<div class="content">
-            		<div class="title">Suites</div>
-                	<div class="name">
-                		<h3><a href="presidential-suite.html" title="Presidential Suite" class="stayType">Presidential Suite</a></h3>
-                	</div><hr>
-                	<div class="description">
-                    	<ul class="descriptUL">
-                        	<li><b>Room Size:</b>  360 sqm.</li>
-                        	<li><b>View:</b>  Ocean view</li>
-                        	<li><b>Ideal For:</b>  2 adults and 2 children or 4 adults</li>
-                        	<li><b>Bed Type(s):</b>  King bed,Twin beds</li>
-                        	<li><b>Inter-connecting Rooms:</b>  Not Available</li>
-                    	</ul>
+            	</div>
+			</div>
+        	<br><br>
+<!--suite3-->
+        	<div class="row rowRoom">
+				<div class="col-12 col-lg-6 order-1 order-lg-2">
+            		<a href="#" title="Presidential Suite"><img src="img/thumb-presidential-suite.jpg" class="img"></a>
+            	</div>
+            	<div class="col-12 col-lg-6 order-2 order-lg-1">
+            		<div class="content">
+            			<div class="title">Suites</div>
+                		<div class="name">
+                			<h3><a href="#" title="Presidential Suite" class="stayType">Presidential Suite</a></h3>
+                		</div><hr>
+                		<div class="description">
+                    		<ul class="descriptUL">
+                        		<li><b>Room Size:</b>  360 sqm.</li>
+                        		<li><b>View:</b>  Ocean view</li>
+                        		<li><b>Ideal For:</b>  2 adults and 2 children or 4 adults</li>
+                        		<li><b>Bed Type(s):</b>  King bed,Twin beds</li>
+                        		<li><b>Inter-connecting Rooms:</b>  Not Available</li>
+                    		</ul>
+               			</div>
+               			<div class="button">
+                        	<a class="btn" href="#" title="View Presidential Suite" style="background-color: #C81168">View</a>
+                        	<a class="btn" href="#" title="Book Presidential Suite">Book Now</a>
+                    	</div>
                		</div>
-               		<div class="button">
-                        <a class="btn" href="presidential-suite.html" title="View Presidential Suite" style="background-color: #C81168">View</a>
-                        <a class="btn" href="#" title="Book Presidential Suite">Book Now</a>
-                    </div>
-               	</div>
+            	</div>
             </div>
-            <div class="col">
-            	<a href="presidential-suite.html" title="Presidential Suite"><img src="img/thumb-presidential-suite.jpg" class="img"></a>
-            </div>
-        </div>
-
-    </div>
-</section>
-
+		</div>
+	</section>
+</div>
 <?php
 include_once "footerHTML.php";
 ?>
