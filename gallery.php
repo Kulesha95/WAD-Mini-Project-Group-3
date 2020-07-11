22ac757ea0bbd2bc6dd6d157dbf936617434af19
nimmijayamali-034 <54774133+nimmijayamali-034@users.noreply.github.com>
2020-07-11 16:47:25 +0530

    gallery part1

--- gallery.php
+++ gallery.php
@@ -4,7 +4,7 @@ include_once "navigation.php";
 ?>
 <div class="container bg-white">
 
-<section class="gallery_details section-gap container">
+<section class="gallery_details section-gap ">
 <div class="row justify-content-center">
             <span class="justify-content-between text-color-purple mr-3 d-none d-md-block">__________________</span>
             <h2 class="headingGallery">GALLERY</h2>
@@ -12,42 +12,54 @@ include_once "navigation.php";
         </div>
     </section>
       
-<div class="slidershow middle">
-   	<div class="slides">
-   		<input type="radio" name="r" id="r1" checked>
-   		  <input type="radio" name="r" id="r2">
-   		  <input type="radio" name="r" id="r3">
-   		  <input type="radio" name="r" id="r4">
-   		  <input type="radio" name="r" id="r5">
+
+
+   <div class="row verTex">
+        <h3> B <br>
+             E <br>
+             A &nbsp&nbsp&nbsp I<br>
+             U &nbsp&nbsp&nbsp N<br>
+             T &nbsp&nbsp&nbsp S<br>
+             Y &nbsp&nbsp&nbsp  I<br>
+             &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp D <br>
+             &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp E <br>
+        </h3>
+
+ <div class="slidershow middle">
+    <div class="slides">
+      <input type="radio" name="r" id="r1" checked>
+        <input type="radio" name="r" id="r2">
+        <input type="radio" name="r" id="r3">
+        <input type="radio" name="r" id="r4">
+        <input type="radio" name="r" id="r5">
        
        <div class="slide s1">
-       	<img src="img\gallery\inside1.jpg" alt="">
+        <img src="img\gallery\inside1.jpg" alt="">
        </div>
        <div class="slide ">
-       	<img src="img\gallery\inside2.jpg" alt="">
+        <img src="img\gallery\inside2.jpg" alt="">
        </div>
        <div class="slide ">
-       	<img src="img\gallery\inside3.jpg" alt="">
+        <img src="img\gallery\inside3.jpg" alt="">
        </div>
        <div class="slide ">
-       	<img src="img\gallery\inside4.jpg" alt="">
+        <img src="img\gallery\inside4.jpg" alt="">
        </div>
        <div class="slide ">
-       	<img src="img\gallery\inside5.jpg" alt="">
+        <img src="img\gallery\inside5.jpg" alt="">
        </div>
 
-   	</div>
+    </div>
 
     <div class="navigation">
-    	<label for="r1" class="bar"></label>
-    	<label for="r2" class="bar"></label>
-    	<label for="r3" class="bar"></label>
-    	<label for="r4" class="bar"></label>
-    	<label for="r5" class="bar"></label>
+      <label for="r1" class="bar"></label>
+      <label for="r2" class="bar"></label>
+      <label for="r3" class="bar"></label>
+      <label for="r4" class="bar"></label>
+      <label for="r5" class="bar"></label>
     </div>
 
    </div>
-
 </div>
 
 
