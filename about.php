ca7a6eb5f67c5528c72457b679a64fe0f6332ddc
Dinushika Sandamali <66713238+Dinushika916@users.noreply.github.com>
2020-07-13 10:30:33 +0530

    about code spaces adjusted

--- about.php
+++ about.php
@@ -5,151 +5,138 @@ include_once "navigation.php";
 
 <!-- HTML code goes here -->
 <body >
-<div class="container bg-white">
-    <section class="about_details section-gap">
-        <div class="row justify-content-center">
-            <span class="justify-content-between text-color-purple mr-3 d-none d-md-block">__________________</span>
-            <h2 class="heading">About Us</h2>
-            <span class="justify-content-between text-color-purple ml-3 d-none d-md-block">__________________</span>
-        </div>
-    </section>
-
-  
-    <div>
-    <div class="about-image"></div>
-    </div>
-
-    <br>
-    <br>
-    
-    <div>
-      <section class="index">
-        <div>
-            <p id="about1">
-    		Avenra Hotels is a public quoted company which entered the leisure industry with a commitment to build and manage a chain of distinctive hotels and resorts that are benchmarked against the most coveted in the world. Avenra intends to provide the most exceptional and memorable hospitality in Sri Lanka by exceeding guest expectations and by creating a travel experience like no other.
-            </p>
-
-            <p id="about2">
-            Avenra currently owns three properties in Sri Lanka.Avenra Colombo is a 90 room hotel situated in the heart of Colombo. Ceremoniously opened in 2014, Avenra Negombo is a 5 star resort in the West Coast of Sri Lanka comprising of 150 rooms with luxury amenities. The Steuart by Avenra is the latest luxury Boutique Business Hotel comprising of 50 rooms, situated on eight floors capturing the mystery of Galle, embedded by history dating back to 1835.
-            </p>
-        </div>
-
-        <div>
-        	<h5><b>VISION</b></h5>
-        	<p id="about_vision">Our vision is to be the ultimate hospitality brand in Sri Lanka.</p>
-
-        </div>
-
-        <div>    
-            <h5><b>MISSION</b></h5>
-
-            <p id="about_mission">Our mission is to own and manage hotels and resorts in Sri Lanka where we can deliver exceptional hospitality by going beyond the call of duty and extending a personalized, friendly and professional service at every given opportunity.</p>
-        </div>
-
-
-    </section>
-
-    <hr>
-
-
-    <section class="label">
-	    <div class="row">
-            <div class="column col-6 col-md-3" >
-            	 <span class="fa-stack fa-4x text-light" >
-                    <i class="fa fa-circle fa-stack-2x icon-background"></i>
-                    <i class="fa fa-bed fa-stack-1x"></i>
-                 </span>
-                 <h2>200</h2>
-                 <h3 id="about_list">Rooms</h3>
-                
-            </div>
-            <div class="column col-6 col-md-3">
-            	    <span class="fa-stack fa-4x text-light">
-                    <i class="fa fa-circle fa-stack-2x icon-background"></i>
-                    <i class="fa fa-bath fa-stack-1x"></i>
-                    </span>
-                    <h2>80</h2>
-                    <h3 id="about_list"> Bathrooms </h3>
-            </div>
-            <div class="column col-6 col-md-3 ">
-            	<span class="fa-stack fa-4x text-light">
-                    <i class="fa fa-circle fa-stack-2x icon-background"></i>
-                    <i class="fa fa-car fa-stack-1x"></i>
-                 </span>
-                 <h2>2+</h2>
-                 <h3 id="about_list"> Parks </h3>
-            </div>
-            <div class="column col-6 col-md-3 ">
-            	<span class="fa-stack fa-4x text-light">
-                    <i class="fa fa-circle fa-stack-2x icon-background"></i>
-                    <i class="fa fa-map-marker fa-stack-1x"></i>
-                 </span>
-                 <h2>2000+</h2>
-                 <h3 id="about_list"> Size in SQFT </h3>
-            </div>
-        </div>
-        </section>
-
-        <hr>
-        <br>
-        <br>
-       
-    
-    	<section class="management_section">
+	
+ <div class="container bg-white">
+        <section class="about_details section-gap">
             <div class="row justify-content-center">
-                <span class="justify-content-between text-color-purple mr-3 d-none d-md-block">__________________</span>
-                <h2 class="heading">Our Management</h2>
-                <span class="justify-content-between text-color-purple ml-3 d-none d-md-block">__________________</span>
+                <span class="justify-content-between text-color-purple mr-3 d-none d-md-block">______</span>
+                <h2 class="heading">About Us</h2>
+                <span class="justify-content-between text-color-purple ml-3 d-none d-md-block">______</span>
             </div>
-        	<div class="row mt-5">
-        	    <div class="col-12 col-md-6 mb-3">
+        </section>
+        <div>
+            <div class="about-image mb-5"></div>
+        </div>
+        <div>
+            <section class="index section-gap">
+                <div>
+                    <p id="about1">
+                        Avenra Hotels is a public quoted company which entered the leisure industry with a commitment to
+                        build and manage a chain of distinctive hotels and resorts that are benchmarked against the most
+                        coveted in the world. Avenra intends to provide the most exceptional and memorable hospitality
+                        in Sri Lanka by exceeding guest expectations and by creating a travel experience like no other.
+                    </p>
+                    <p id="about2">
+                        Avenra currently owns three properties in Sri Lanka.Avenra Colombo is a 90 room hotel situated
+                        in the heart of Colombo. Ceremoniously opened in 2014, Avenra Negombo is a 5 star resort in the
+                        West Coast of Sri Lanka comprising of 150 rooms with luxury amenities. The Steuart by Avenra is
+                        the latest luxury Boutique Business Hotel comprising of 50 rooms, situated on eight floors
+                        capturing the mystery of Galle, embedded by history dating back to 1835.
+                    </p>
+                </div>
+                <div class="mt-5">
+                    <h5><b>VISION</b></h5>
+                    <p id="about_vision">Our vision is to be the ultimate hospitality brand in Sri Lanka.</p>
 
-			    	    <h4><b>Chairman</b></h4>
-			    	    <ul>
-			    	    	<li id="management_list">Mr.Nimal Fernando</li>
-			    	    </ul>
-			    	    <h4><b>Directors</b></h4>
-			    	    <ul>
-			    	    	<li id="management_list">Mr.Ajantha Perera</li>
-			    	    	<li id="management_list">Ms.Dilini Silva</li>
-			    	    	<li id="management_list">Mr.Oshan Peris</li>
-			    	    	<li id="management_list">Mr.Roshitha Jayaweera</li>
-			    	    	<li id="management_list">Mr.Ranjith Kahandawa</li>
-			    	    	<li id="management_list">Ms.Keshini Rajapakse</li>
-			            </ul>
-			            <h4><b>Management Team</b></h4>
-			            <ul>
-			            <li id="management_list"><b>Mr. Nimal Fernando Director/ CEO Avenra Hotels</b></li>
-			            </ul>
-			            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>E-mail</b>: chandana@avenrahotels.com</p>
-			            <ul>
-			            <li id="management_list"><b>Mr. Hemantha Ratnayake Director Operations Avenra Hotels</b></li>
-			            </ul>
-			            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>E-mail</b>: hemantha@avenrahotels.com</p>
-			            <ul>
-			            <li id="management_list"><b>Mr. Mani Sugathapala Director Sales and Marketing </b></li>
-			            </ul>
-			            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Direct</b>:  +94114376100 </p>
-			            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Mobile</b>:  +94776890789 </p>
-			            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>E-mail</b>:  Mani@avenrahotels.com </p>
+                </div>
+                <div class="mt-5">
+                    <h5><b>MISSION</b></h5>
+                    <p id="about_mission">Our mission is to own and manage hotels and resorts in Sri Lanka where we can
+                        deliver exceptional hospitality by going beyond the call of duty and extending a personalized,
+                        friendly and professional service at every given opportunity.</p>
+                </div>
+            </section>
+            <hr>
+            <section class="label section-gap">
+                <div class="row">
+                    <div class="column col-6 col-md-3">
+                        <span class="fa-stack fa-4x text-light">
+                            <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                            <i class="fa fa-bed fa-stack-1x"></i>
+                        </span>
+                        <h2>200</h2>
+                        <h3 id="about_list">Rooms</h3>
 
-			        </div>
-			        <div class="col-md-6">
-			        	<br>
-			        	<br>
-			        	<br>
-			        	<br>
-			        	<br>
+                    </div>
+                    <div class="column col-6 col-md-3">
+                        <span class="fa-stack fa-4x text-light">
+                            <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                            <i class="fa fa-bath fa-stack-1x"></i>
+                        </span>
+                        <h2>80</h2>
+                        <h3 id="about_list"> Bathrooms </h3>
+                    </div>
+                    <div class="column col-6 col-md-3 ">
+                        <span class="fa-stack fa-4x text-light">
+                            <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                            <i class="fa fa-car fa-stack-1x"></i>
+                        </span>
+                        <h2>2+</h2>
+                        <h3 id="about_list"> Parks </h3>
+                    </div>
+                    <div class="column col-6 col-md-3 ">
+                        <span class="fa-stack fa-4x text-light">
+                            <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                            <i class="fa fa-map-marker fa-stack-1x"></i>
+                        </span>
+                        <h2>2000+</h2>
+                        <h3 id="about_list"> Size in SQFT </h3>
+                    </div>
+                </div>
+            </section>
+            <hr>
 
-			            <div class="about_image_2"></div>
+            <section class="management_section section-gap">
+                <div class="row justify-content-center">
+                    <span
+                        class="justify-content-between text-color-purple mr-3 d-none d-md-block">______</span>
+                    <h2 class="heading">Our Management</h2>
+                    <span
+                        class="justify-content-between text-color-purple ml-3 d-none d-md-block">______</span>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-12 col-md-6">
+
+                        <h4><b>Chairman</b></h4>
+                        <ul>
+                            <li id="management_list">Mr.Nimal Fernando</li>
+                        </ul>
+                        <h4><b>Directors</b></h4>
+                        <ul>
+                            <li id="management_list">Mr.Ajantha Perera</li>
+                            <li id="management_list">Ms.Dilini Silva</li>
+                            <li id="management_list">Mr.Oshan Peris</li>
+                            <li id="management_list">Mr.Roshitha Jayaweera</li>
+                            <li id="management_list">Mr.Ranjith Kahandawa</li>
+                            <li id="management_list">Ms.Keshini Rajapakse</li>
+                        </ul>
+                        <h4><b>Management Team</b></h4>
+                        <ul>
+                            <li id="management_list"><b>Mr. Nimal Fernando Director/ CEO Avenra Hotels</b></li>
+                        </ul>
+                        <p class="pl-5"><b>E-mail</b>:
+                            chandana@avenrahotels.com</p>
+                        <ul>
+                            <li id="management_list"><b>Mr. Hemantha Ratnayake Director Operations Avenra Hotels</b>
+                            </li>
+                        </ul>
+                        <p class="pl-5"><b>E-mail</b>:
+                            hemantha@avenrahotels.com</p>
+                        <ul>
+                            <li id="management_list"><b>Mr. Mani Sugathapala Director Sales and Marketing </b></li>
+                        </ul>
+                        <p class="pl-5"><b>Direct</b>: +94114376100 </p>
+                        <p class="pl-5"><b>Mobile</b>: +94776890789 </p>
+                        <p class="pl-5"><b>E-mail</b>: Mani@avenrahotels.com
+                        </p>
+                    </div>
+                    <div class="col-md-6 mt-3">
+                        <div class="about_image_2"></div>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
-        
-    </section>
-
-
-    
-</div>
 </body>
 
 <?php
