b6aa6cecfd591d6859d8dee83b37206fddc1df42
Kulesha Rashmini <64342403+Kulesha95@users.noreply.github.com>
2020-07-17 21:23:26 +0530

    insert backend  function for save contact  message

--- admin/Message/addController.php
+++ admin/Message/addController.php
@@ -1,6 +1,9 @@
 <?php
-include_once '..#config.php';
+include_once '../config.php';
 
+$insertSql = "INSERT INTO `message` (`id`,`name`,`email`,`subject`,`message`,`user_id`) VALUES(null,'$_POST[name]','$_POST[email]','$_POST[message]','$_POST[user_id]')";
+$insertQuery=mysqli_query($connection,$insertSql);
+header("location:../../contact.php");
 
 
 
