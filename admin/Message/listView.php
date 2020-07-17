69ebf1c1bce52fa5728e6df39c3309486c47a20f
Kulesha Rashmini <64342403+Kulesha95@users.noreply.github.com>
2020-07-17 23:12:38 +0530

    Message list view backend interface created

---
+++ admin/Message/listView.php
@@ -0,0 +1,55 @@
+<?php
+    include_once '../config.php';
+    include_once '../headerHTML.php';
+    include_once "../function.php";
+?>
+<div class="container jumbotron bg-white shadow mt-4 col-11">
+    <div class="card">
+        <div class="card-header bg-white">
+            <div class="row">
+                <i class="fas fa-envelope-circle mx-2 my-auto fa-2x"></i>
+                <h4 class="ml-2 my-auto">Messages List</h4>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover table-sm">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Message</th>
+                            <th>Subject</th>
+                            <th>User</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                <?php
+                    $listSql='select * from `Message`';
+                    $listResult=mysqli_query($connection,$listSql);
+                    while($listRow=mysqli_fetch_assoc($listResult)){
+                ?>            
+                        <tr>
+                            <td><?php echo $listRow['name'];?></td>
+                            <td><?php echo $listRow['email'];?></td>
+                            <td><?php echo $listRow['message'];?></td>
+                            <td><?php echo $listRow['subject'];?></td>
+                            <td><?php echo user($listRow['user_id']);?></td>
+                            <td>
+                                <div class="btn-group" role="group">
+                                    <!-- <a class="btn btn-primary btn-sm" href="editView.php?id=<?php echo $listRow['id'];?>" onclick="return confirm('Are you sure you want to edit this record?')"><i class="fas fa-edit mr-2"></i>Edit</a> -->
+                                    <a class="btn btn-danger btn-sm" href="deleteController.php?id=<?php echo $listRow['id'];?>" onclick="return confirm('Are you sure you want to delete this record?')"><i class="fas fa-trash-alt mr-2"></i>Delete</a>
+                                </div>
+                            </td>
+                        </tr>
+                <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    include_once '../footerHTML.php';
+?>
--- admin/function.php
+++ admin/function.php
@@ -1,16 +1,28 @@
 <?php
 
 function feature($id){
-    $featureSql="select * from feature";
+    $featureSql="select * from feature where id= '$id'";
     $featureResult=mysqli_query($GLOBALS['connection'],$featureSql);
     $featureData=mysqli_fetch_assoc($featureResult);
     return $featureData['name'];
 }
 
 function amenity($id){
-    $amenitySql="select * from amenity";
+    $amenitySql="select * from amenity where id= '$id'";
     $amenityResult=mysqli_query($GLOBALS['connection'],$amenitySql);
     $amenityData=mysqli_fetch_assoc($amenityResult);
     return $amenityData['name'];
+}
+
+function user($id){
+    $userSql="select * from user where id= '$id'";
+    $userResult=mysqli_query($GLOBALS['connection'],$userSql);
+    $userData=mysqli_fetch_assoc($userResult);
+    if($userData=mysqli_fetch_assoc($userResult)){
+        return $userData['name'];
+    }else{
+        return "Guest";
+    }
+    return $userData['name'];
 }
 ?>
