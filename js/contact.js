a2fdcd9b24794968c290b627ece0cae548ba9e15
Kulesha Rashmini <64342403+Kulesha95@users.noreply.github.com>
2020-07-17 18:31:55 +0530

    java script function edited

--- js/contact.js
+++ js/contact.js
@@ -1,26 +1,27 @@
 
-$(window).scroll(function() {
-    var hT = $('#review-card-container').offset().top,
-        hH = $('#review-card-container').outerHeight(),
-        wH = $(window).height(),
-        wS = $(this).scrollTop();
-    if (wS > (hT + hH - wH)) {
-        var divs = document.querySelectorAll('.card');
-        for (var i = 0; i < divs.length; i++) {
-            divs[i].classList.add('review-card');
+$(window).scroll(function () {
+    if ($(".review-card-before").length) {
+        let offsetTop = $('#review-card-container').offset().top,
+            windowHeight = $(window).height(),
+            scrollTop = $(this).scrollTop();
+        if (scrollTop > (offsetTop-windowHeight+100)) {
+            var divs = document.querySelectorAll('.review-card-before');
+            for (var i = 0; i < divs.length; i++) {
+                divs[i].classList.add('review-card');
+            }
         }
     }
 });
 
 
-function rate(rating){
+function rate(rating) {
     for (let index = 1; index <= 5; index++) {
-        $("#star"+index).removeClass("fas");        
-        $("#star"+index).addClass("far");        
+        $("#star" + index).removeClass("fas");
+        $("#star" + index).addClass("far");
     }
     for (let index = 1; index <= rating; index++) {
-        $("#star"+index).removeClass("far");        
-        $("#star"+index).addClass("fas");        
+        $("#star" + index).removeClass("far");
+        $("#star" + index).addClass("fas");
     }
     $("#ratings").val(rating);
 }
