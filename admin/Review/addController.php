e2c83325762d28af475b2a94eae82210e4f36d94
Kulesha Rashmini <64342403+Kulesha95@users.noreply.github.com>
2020-07-18 10:58:55 +0530

    sql query issue is fixed

--- admin/Review/addController.php
+++ admin/Review/addController.php
@@ -3,8 +3,9 @@ include_once '../config.php';
 $date= date("Y-m-d");
 $time= date("H:i:s");
 
-$insertSql = "INSERT INTO `message` (`id`,`rate`,`review`,`date`,`time`,`user_id`) VALUES(null,'$_POST[rate]','$_POST[review]','$date','$time','$_POST[user_id]')";
+$insertSql = "INSERT INTO `review` (`id`,`rate`,`review`,`date`,`time`,`user_id`) VALUES(null,'$_POST[rate]','$_POST[review]','$date','$time','$_POST[user_id]')";
 $insertQuery=mysqli_query($connection,$insertSql);
+
 header("location:../../contact.php");
 
 
