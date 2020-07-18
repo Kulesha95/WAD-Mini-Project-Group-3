c2d09e6065df378b37f90ea4c166eb924a7ec0fc
Kulesha Rashmini <64342403+Kulesha95@users.noreply.github.com>
2020-07-18 11:04:46 +0530

    Reviews list table is created

---
+++ admin/Review/listView.php
@@ -0,0 +1,55 @@
+<?php
+    include_once '../config.php';
+    include_once '../headerHTML.php';
+    include_once "../function.php";
+?>
+<div class="container jumbotron bg-white shadow mt-4 col-11">
+    <div class="card">
+        <div class="card-header bg-white">
+            <div class="row">
+                <i class="fas fa-award mx-2 my-auto fa-2x"></i>
+                <h4 class="ml-2 my-auto">Reviews List</h4>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover table-sm">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Rate</th>
+                            <th>Review</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>User</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                <?php
+                    $listSql='select * from `review`';
+                    $listResult=mysqli_query($connection,$listSql);
+                    while($listRow=mysqli_fetch_assoc($listResult)){
+                ?>            
+                        <tr>
+                            <td><?php echo $listRow['rate'];?></td>
+                            <td><?php echo $listRow['review'];?></td>
+                            <td><?php echo $listRow['date'];?></td>
+                            <td><?php echo $listRow['time'];?></td>
+                            <td><?php echo user($listRow['user_id']);?></td>
+                            <td>
+                                <div class="btn-group" role="group">
+                                    <!-- <a class="btn btn-primary btn-sm" href="editView.php?id=<?php echo $listRow['id'];?>" onclick="return confirm('Are you sure you want to edit this record?')"><i class="fas fa-edit mr-2"></i>Edit</a> -->
+                                    <a class="btn btn-danger btn-sm" href="deleteController.php?id=<?php echo $listRow['id'];?>" onclick="return confirm('Are you sure you want to delete this record?')"><i class="fas fa-trash-alt mr-2"></i>Delete</a>
+                                </div>
+                            </td>
+                        </tr>
+                <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    include_once '../footerHTML.php';
+?>
