aefaf41c5c6bdf14476c6e173cdbd68ae37d38b1
Kulesha Rashmini <64342403+Kulesha95@users.noreply.github.com>
2020-07-20 21:28:57 +0530

    Review user login validation created

--- admin/Review/addController.php
+++ admin/Review/addController.php
@@ -1,12 +1,26 @@
 <?php
+session_start();
 include_once '../config.php';
-$date= date("Y-m-d");
-$time= date("H:i:s");
+if(isset($_SESSION['user_id'])){
+    $date= date("Y-m-d");
+    $time= date("H:i:s");
+    
+    $insertSql = "INSERT INTO `review` (`id`,`rate`,`review`,`date`,`time`,`user_id`) VALUES(null,'$_POST[rate]','$_POST[review]','$date','$time','$_POST[user_id]')";
+    $insertQuery=mysqli_query($connection,$insertSql);
+    header("location:../../contact.php");
+}else{
+    ?>
+     <script>
+     alert("Please login to submit your Review... ");
+     window.location= "../../contact.php";
+     </script>
+     
+    <?php
+   
+}
 
-$insertSql = "INSERT INTO `review` (`id`,`rate`,`review`,`date`,`time`,`user_id`) VALUES(null,'$_POST[rate]','$_POST[review]','$date','$time','$_POST[user_id]')";
-$insertQuery=mysqli_query($connection,$insertSql);
 
-header("location:../../contact.php");
+
 
 
 
