068484923d80424d2ae4a8b66719154797e65bf2
Kulesha Rashmini <64342403+Kulesha95@users.noreply.github.com>
2020-07-21 21:37:03 +0530

    Cms data save function crated

--- admin/Cms/addController.php
+++ admin/Cms/addController.php
@@ -1,7 +1,24 @@
-<?php 
-include_once '../config.php';
-
-$insertSql="INSERT INTO `cms` (`id`,`key`,`value`,`group_code`,`page`) VALUES(null,'$_POST[key]','$_POST[value]','$_POST[group_code]','$_POST[page]')";
-$insertQuery=mysqli_query($connection,$insertSql);
-header("location:listView.php");
-?>
+<?php
+include_once "../config.php";
+$groupCountSql = "SELECT distinct `group_code` FROM cms WHERE `group_code` LIKE '$_POST[group_code]%' AND `page`='$_POST[page]' GROUP BY `group_code`";
+$groupCountResult = mysqli_query($connection, $groupCountSql);
+$groupCode = $_POST['group_code'] . (mysqli_num_rows($groupCountResult) + 1);
+foreach ($_POST as $key => $value) {
+    if ($key == "page" || $key == "type" || $key == "group_code") {
+        continue;
+    } else if ($_POST['type'] == "single") {
+        $cmsSql = "SELECT * FROM `cms` WHERE `page`='$_POST[page]' AND `key`='$key'";
+        $cmsResult = mysqli_query($connection, $cmsSql);
+        if ($cmsData = mysqli_fetch_assoc($cmsResult)) {
+            $updateSql = "UPDATE `cms` SET `key`='$key',`value`='$value' WHERE `id`='$cmsData[id]'";
+            $updateQuery = mysqli_query($connection, $updateSql);
+        } else {
+            $insertSql = "INSERT INTO `cms` (`id`,`key`,`value`,`group_code`,`page`) VALUES(null,'$key','$value','$groupCode','$_POST[page]')";
+            $insertQuery = mysqli_query($connection, $insertSql);
+        }
+    } else {
+        $insertSql = "INSERT INTO `cms` (`id`,`key`,`value`,`group_code`,`page`) VALUES(null,'$key','$value','$groupCode','$_POST[page]')";
+        $insertQuery = mysqli_query($connection, $insertSql);
+    }
+}
+header("location:".$_SERVER['HTTP_REFERER']);
